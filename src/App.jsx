77c910c1da09de75ada525ba9bbd3e6dc46bc43e
RoniPG <ronigmn@gmail.com>
2024-11-21 19:28:41 +0100

    Iteration 1 | Lift state up to the common parent

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,20 @@ import ExponentThree from "./components/ExponentThree";
 import ExponentFour from "./components/ExponentFour";
 import ExponentFive from "./components/ExponentFive";
 import ExponentSix from "./components/ExponentSix";
+import { useState } from "react";
 
 
 function App () {
+  const [count, setCount] = useState(0);
+
+  const decrement = () => setCount((prevCount) => prevCount - 1);
+  const increment = () => setCount((prevCount) => prevCount + 1);  
+  
   return (
     <div className="App">
       <h2><em>Counter</em></h2>
   
-      <Counter/>
+      <Counter count={count} decrement={decrement} increment={increment}/>
 
       <br />
       <h2><em>Exponents</em></h2>
